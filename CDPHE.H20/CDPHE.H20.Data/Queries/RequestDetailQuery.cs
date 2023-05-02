d847b9f016caee462fc3be709947eaee8b04ddc2
Matthew Wilson <mwilson@istonish.com>
2023-05-02 16:10:42 -0400

    SQL fix

--- CDPHE.H20/CDPHE.H20.Data/Queries/RequestDetailQuery.cs
+++ CDPHE.H20/CDPHE.H20.Data/Queries/RequestDetailQuery.cs
@@ -10,14 +10,14 @@ namespace CDPHE.H20.Data.Queries
     {
         public static string GetRequestDetailsByRequestId()
         {
-            string sql = "SELECT Request.Id, Request.Status, Request.CreatedAt, Facility.Name AS Facility, [User].FirstName + ' ' + [User].LastName AS Provider, [User].Email, [User].Phone, Facility.WQCID, Facility.Address1, Facility.Address2, Facility.City, Facility.State, Facility.ZipCode FROM Request INNER JOIN Facility ON Request.FacilityId = Facility.Id INNER JOIN [User] ON Request.Id = [User].id WHERE Request.Id = @ID" +
+            string sql = "SELECT Request.Id, Request.Status, Request.CreatedAt, Facility.Name AS Facility, [User].FirstName + ' ' + [User].LastName AS Provider, [User].Email, [User].Phone, Facility.WQCID, Facility.Address1, Facility.Address2, Facility.City, Facility.State, Facility.ZipCode FROM Request INNER JOIN Facility ON Request.FacilityId = Facility.Id INNER JOIN [User] ON Request.UserId = [User].id WHERE Request.Id = @ID" +
                          " SELECT rd.Id AS RequestDetailId, rd.SampleName, rd.InitialSampleDate, rd.SampleResultOperator, rd.InitialSampleResult, rd.FlushSampleDate, rd.FlushResultOperator, rd.FlushSampleResult, rd.RemedialActionId, rd.MaterialCost, rd.MaterialLabor, RemedialAction.Name FROM RequestDetail AS rd INNER JOIN Request AS r ON rd.RequestId = r.Id INNER JOIN RemedialAction ON rd.RemedialActionId = RemedialAction.Id WHERE (r.Id = @ID) AND (rd.IsActive = 1)";
             return sql;
         }
 
         public static string GetRequestsByProviderId()
         {
-            string sql = "SELECT Request.Id AS Id, Facility.Name as Facility, [User].FirstName + ' ' + [User].LastName AS Provider, [User].Email, Request.CreatedAt, Request.Status FROM Request INNER JOIN Facility ON Request.FacilityId = Facility.Id INNER JOIN [User] ON Request.Id = [User].id WHERE Request.UserId = @Id";
+            string sql = "SELECT Request.Id AS Id, Facility.Name as Facility, [User].FirstName + ' ' + [User].LastName AS Provider, [User].Email, Request.CreatedAt, Request.Status FROM Request INNER JOIN Facility ON Request.FacilityId = Facility.Id INNER JOIN [User] ON Request.UserId = [User].id WHERE Request.UserId = @Id";
             return sql;
         }
 
